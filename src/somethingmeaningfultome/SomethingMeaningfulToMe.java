a20ae7bc0814cfb0b45d66eac5b44506586489dd
mirko <mirko@ninja5.d22.lan>
2015-09-15 07:45:41 -0500

    This is our first NetBeans program.
    It's for a loop.

---
+++ src/somethingmeaningfultome/SomethingMeaningfulToMe.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package somethingmeaningfultome;
+
+/**
+ *
+ * @author mirko
+ */
+public class SomethingMeaningfulToMe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    for(int i = 10; i >= 0; i--) {
+    System.out.println("Hello, period " + i + "!");
+    }
+    
+   
+    
+    }
+    
+
+}
